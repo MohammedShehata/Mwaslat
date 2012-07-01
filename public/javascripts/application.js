6062ec0e00af2667b6536b81028f3ffded1aa0a1
Ahmed Fathy <sininenjj@gmail.com>
2012-07-02 01:35:24 +0200

    USER GUIDE

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,59 +8,80 @@ var template_2 = "route_sub_routes_attributes_index_dest_attributes_";
 var to_replace = new RegExp("index", "g");
 
 $(document).ready(function() {
+	$("#registerGuideData").hide();
+	$("#addNodeGuideData").hide();
+	$("#addRouteGuideData").hide();
+	$("#searchGuideData").hide();
 	$('.dropdown-toggle').dropdown();
-	$("#admin_routes_table").dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-		"sPaginationType": "bootstrap",
-		"oLanguage": {
-			"sLengthMenu": "_MENU_ records per page"
+	$("#admin_routes_table").dataTable({
+		"sDom" : "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sPaginationType" : "bootstrap",
+		"oLanguage" : {
+			"sLengthMenu" : "_MENU_ records per page"
 		},
 		"bSort" : false,
-		"aoColumnDefs": [{ "bSearchable": false, "aTargets": [5, 7] }],
-		"bPaginate": false,
-		"bInfo": false
-	} );
-	
-	$("#user_routes_table").dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-		"sPaginationType": "bootstrap",
-		"oLanguage": {
-			"sLengthMenu": "_MENU_ records per page"
-		},
-		"aoColumnDefs":[{ "bSortable": false, "aTargets": [5,6] }, { "bSearchable": false, "aTargets": [5,6] }],
-		"bPaginate": false,
-		"bInfo": false
-	} );
-	
-	$("#admin_nodes_table").dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-		"sPaginationType": "bootstrap",
-		"oLanguage": {
-			"sLengthMenu": "_MENU_ records per page"
-		},
-		"bSort": false,
-		 "aoColumnDefs": [{ "bSearchable": false, "aTargets": [ 3 ] }],
-		"bPaginate": false,
-		"bInfo": false
-	} );
-	
-	$("#user_nodes_table").dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-		"sPaginationType": "bootstrap",
-		"oLanguage": {
-			"sLengthMenu": "_MENU_ records per page"
-		},
-		 "aoColumnDefs":[{ "bSortable": false, "aTargets": [2] }, { "bSearchable": false, "aTargets": [2] }],
-		 "bPaginate": false,
-     	 "bInfo": false
-	} );
-	
-} );
+		"aoColumnDefs" : [{
+			"bSearchable" : false,
+			"aTargets" : [5, 7]
+		}],
+		"bPaginate" : false,
+		"bInfo" : false
+	});
 
-function show_category_description(){
+	$("#user_routes_table").dataTable({
+		"sDom" : "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sPaginationType" : "bootstrap",
+		"oLanguage" : {
+			"sLengthMenu" : "_MENU_ records per page"
+		},
+		"aoColumnDefs" : [{
+			"bSortable" : false,
+			"aTargets" : [5, 6]
+		}, {
+			"bSearchable" : false,
+			"aTargets" : [5, 6]
+		}],
+		"bPaginate" : false,
+		"bInfo" : false
+	});
+
+	$("#admin_nodes_table").dataTable({
+		"sDom" : "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sPaginationType" : "bootstrap",
+		"oLanguage" : {
+			"sLengthMenu" : "_MENU_ records per page"
+		},
+		"bSort" : false,
+		"aoColumnDefs" : [{
+			"bSearchable" : false,
+			"aTargets" : [3]
+		}],
+		"bPaginate" : false,
+		"bInfo" : false
+	});
+
+	$("#user_nodes_table").dataTable({
+		"sDom" : "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sPaginationType" : "bootstrap",
+		"oLanguage" : {
+			"sLengthMenu" : "_MENU_ records per page"
+		},
+		"aoColumnDefs" : [{
+			"bSortable" : false,
+			"aTargets" : [2]
+		}, {
+			"bSearchable" : false,
+			"aTargets" : [2]
+		}],
+		"bPaginate" : false,
+		"bInfo" : false
+	});
+
+});
+function show_category_description() {
 	var category = $("#node_category").val();
 	var description = $("#category_description");
-	switch(category){
+	switch(category) {
 		case "District":
 			description.html("ewqs");
 			break;
@@ -112,21 +133,19 @@ function show_category_description(){
 }
 
 // 0. 1. 2. 3. ...
-function remove_child(sub_route_index){
-	if(sub_route_index < sub_routes_ids.length && (sub_route_index >= 0)){
+function remove_child(sub_route_index) {
+	if(sub_route_index < sub_routes_ids.length && (sub_route_index >= 0)) {
 		var div_index = sub_routes_ids[sub_route_index];
-		var div = $("#"+div_index.toString().replace(".", "\\."));
+		var div = $("#" + div_index.toString().replace(".", "\\."));
 		div.hide();
-		$("#"+ template_1.replace(to_replace, div_index).replace(".", "\\.") +"_destroy").val("1");
-		if(sub_route_index == 0){			// if first node, then hide next duration field
+		$("#" + template_1.replace(to_replace, div_index).replace(".", "\\.") + "_destroy").val("1");
+		if(sub_route_index == 0) {// if first node, then hide next duration field
 			sub_routes_ids.shift();
 			hide_time_fields(sub_routes_ids[0]);
-		}
-		else{
+		} else {
 			sub_routes_ids.splice(sub_route_index, 1);
 		}
-	}
-	else{
+	} else {
 		alert("Problem");
 	}
 	rename_stops(sub_route_index);
@@ -134,42 +153,42 @@ function remove_child(sub_route_index){
 	alert(used_ids.toString());
 }
 
-function clear_name_field(sub_route_index){
+function clear_name_field(sub_route_index) {
 	var node_name_id = "#" + template_2.replace(to_replace, sub_routes_ids[sub_route_index]).replace(".", "\\.") + "name";
 	$(node_name_id).val("");
 	$(node_name_id).removeAttr("disabled");
 }
 
 // 0. 1. 2. 3. .....
-function add_child(sub_route_index){
+function add_child(sub_route_index) {
 	// "sub_route_element" is a variable set using the form builder in the .html.erb file
 	var sub_route_instance = sub_route_element.toString();
 	var regexp = new RegExp("sub_route_index", "g");
 	var new_sub_route_id;
 	var sub_route_before;
-	if(sub_route_index == 0){				// adds a new source stop
-		new_sub_route_id = get_new_id(0);		// (-1 + index)/2 => new source id
+	if(sub_route_index == 0) {// adds a new source stop
+		new_sub_route_id = get_new_id(0);
+		// (-1 + index)/2 => new source id
 		alert(new_sub_route_id);
-		show_time_fields(sub_routes_ids[0]);			// show the old source time fields
-		sub_route_before = $("#" + sub_routes_ids[0]);	// old source element
+		show_time_fields(sub_routes_ids[0]);
+		// show the old source time fields
+		sub_route_before = $("#" + sub_routes_ids[0]);
+		// old source element
 		sub_route_before.before(sub_route_instance.replace(regexp, new_sub_route_id));
 		sub_routes_ids.unshift(new_sub_route_id);
 		hide_time_fields(sub_routes_ids[0]);
-	}
-	else{
-		if(sub_route_index == sub_routes_ids.length){		// adds a new destination stop
+	} else {
+		if(sub_route_index == sub_routes_ids.length) {// adds a new destination stop
 			new_sub_route_id = get_dest_id();
-			sub_route_before = $("#" + sub_routes_ids[sub_route_index-1]);
+			sub_route_before = $("#" + sub_routes_ids[sub_route_index - 1]);
 			sub_route_before.after(sub_route_instance.replace(regexp, new_sub_route_id));
 			sub_routes_ids.push(new_sub_route_id);
-		}
-		else if (sub_route_index < sub_routes_ids.length && (sub_route_index > 0)){   // adds a new intermediate stop
+		} else if(sub_route_index < sub_routes_ids.length && (sub_route_index > 0)) {// adds a new intermediate stop
 			new_sub_route_id = get_new_id(sub_route_index);
-			sub_route_before = $("#" + sub_routes_ids[sub_route_index-1]);
+			sub_route_before = $("#" + sub_routes_ids[sub_route_index - 1]);
 			sub_route_before.after(sub_route_instance.replace(regexp, new_sub_route_id));
 			sub_routes_ids.splice(sub_route_index, 0, new_sub_route_id);
-		}
-		else{
+		} else {
 			alert("Problem");
 		}
 	}
@@ -179,21 +198,22 @@ function add_child(sub_route_index){
 }
 
 function rename_stops(start) {
-	for(var i=start; i < sub_routes_ids.length; i++){
-		$("label[for=" + template_2.replace(to_replace, sub_routes_ids[i]).replace(".", "\\.") + "name]").html("Stop " + (i+1));
+	for(var i = start; i < sub_routes_ids.length; i++) {
+		$("label[for=" + template_2.replace(to_replace, sub_routes_ids[i]).replace(".", "\\.") + "name]").html("Stop " + (i + 1));
 	}
 }
 
-// it makes three actions => 
+// it makes three actions =>
 // first : set numbered names of initial stops
 // second : changes ids of the initial children divs
 // third : hides first time field of first child
-function prepare_form(){
+function prepare_form() {
 	// changes ids of sub_routes divs
 	var sub_routes = $("#sub_routes #sub_route_index");
-	for(var i=0; i< sub_routes.length; i++){
+	for(var i = 0; i < sub_routes.length; i++) {
 		sub_routes.eq(i).attr("id", i);
-		sub_routes_ids.push(i);		// add the index to array of ids
+		sub_routes_ids.push(i);
+		// add the index to array of ids
 		used_ids.push(i);
 	}
 	// assigns numbered names of stops
@@ -203,29 +223,30 @@ function prepare_form(){
 }
 
 // shows the time fields of the div of specified id
-function show_time_fields(id){
-	var minutes_key = template_1.replace(to_replace, id).replace(".", "\\.")+"duration_minutes";
+function show_time_fields(id) {
+	var minutes_key = template_1.replace(to_replace, id).replace(".", "\\.") + "duration_minutes";
 	$("#" + minutes_key).parent().parent().show();
 }
 
 // hides the time fields of the div of specified id
-function hide_time_fields(id){
-	var minutes_key = template_1.replace(to_replace, id).replace(".", "\\.")+"duration_minutes";
+function hide_time_fields(id) {
+	var minutes_key = template_1.replace(to_replace, id).replace(".", "\\.") + "duration_minutes";
 	$("#" + minutes_key).parent().parent().hide();
 }
-/* 
+
+/*
  * "isFirst" is set to true when the added node is the first or second node
  * "sub_route_index" represents the ordering of the node
  * other attrs are for node's data
-*/
-function add_selected_node(name, path, id, sub_route_index, isFirst){
-	if(!isFirst){
+ */
+function add_selected_node(name, path, id, sub_route_index, isFirst) {
+	if(!isFirst) {
 		add_child(sub_route_index);
 	}
 	var node_id_template = "#" + template_2.replace(to_replace, sub_routes_ids[sub_route_index]).replace(".", "\\.");
 	$(node_id_template + "name").val(name);
 	$(node_id_template + "path").val(path);
-	$(node_id_template + "id").val(id);	
+	$(node_id_template + "id").val(id);
 	$(node_id_template + "name").attr("disabled", "disabled");
 }
 
@@ -237,37 +258,35 @@ function fillPaths() {
 	}
 }
 
-function get_dest_id(){
-	var new_sub_route_id = sub_routes_ids[sub_routes_ids.length-1] + 1;
-	while(is_used(new_sub_route_id)){
-		new_sub_route_id ++;
+function get_dest_id() {
+	var new_sub_route_id = sub_routes_ids[sub_routes_ids.length - 1] + 1;
+	while(is_used(new_sub_route_id)) {
+		new_sub_route_id++;
 	}
 	used_ids.push(new_sub_route_id);
 	return new_sub_route_id;
 }
 
-function get_new_id(sub_route_index){
+function get_new_id(sub_route_index) {
 	var x, y, new_sub_route_id;
 	var divisor = 2;
-	if(sub_route_index == 0){
+	if(sub_route_index == 0) {
 		x = sub_routes_ids[0];
 		y = -1;
-	}
-	else{
+	} else {
 		x = sub_routes_ids[sub_route_index];
-		y = sub_routes_ids[sub_route_index-1];
+		y = sub_routes_ids[sub_route_index - 1];
 	}
-	if((x + y) == 0){
-		new_sub_route_id = (Math.abs(x) + Math.abs(y))/divisor - Math.abs(x);
-		while(is_used(new_sub_route_id)){
-			divisor ++;
-			new_sub_route_id = (Math.abs(x) + Math.abs(y))/divisor - Math.abs(x);
+	if((x + y) == 0) {
+		new_sub_route_id = (Math.abs(x) + Math.abs(y)) / divisor - Math.abs(x);
+		while(is_used(new_sub_route_id)) {
+			divisor++;
+			new_sub_route_id = (Math.abs(x) + Math.abs(y)) / divisor - Math.abs(x);
 		}
-	}
-	else{
+	} else {
 		new_sub_route_id = (x + y) / divisor;
-		while(is_used(new_sub_route_id)){
-			divisor ++;
+		while(is_used(new_sub_route_id)) {
+			divisor++;
 			new_sub_route_id = (x + y) / divisor;
 		}
 	}
@@ -275,27 +294,61 @@ function get_new_id(sub_route_index){
 	return new_sub_route_id;
 }
 
-function is_used(id){
-	for(var i = 0 ; i < used_ids.length ; i++){
+function is_used(id) {
+	for(var i = 0; i < used_ids.length; i++) {
 		if(used_ids[i] == id)
 			return true;
 	}
 	return false;
 }
 
-function fillNodePath()
-{
-    var overlays = map.getOverlays();
-    document.getElementById("node_path").value = overlays[0].getPointString();
+function fillNodePath() {
+	var overlays = map.getOverlays();
+	document.getElementById("node_path").value = overlays[0].getPointString();
 }
 
 $(function() {
-    $( ".auto" ).autocomplete({
-        source: function( request, response ) {
-            var term = request.term;
-            lastXhr = $.getJSON( "/routes/nodes/districts.json", request, function( data, status, xhr ) {
-                response( data );
-            });
-        }
-    });
+	$(".auto").autocomplete({
+		source : function(request, response) {
+			var term = request.term;
+			lastXhr = $.getJSON("/routes/nodes/districts.json", request, function(data, status, xhr) {
+				response(data);
+			});
+		}
+	});
 });
+function activeLi(element) {
+	liArray = document.getElementById("navlist").childNodes;
+	i = 0;
+	while(liArray[i]) {
+		liArray[i].id = "";
+		i++;
+
+	}
+	element.id = "active";
+}
+
+function openGuideData(index) {
+	if(index == 1) {
+		$("#registerGuideData").show();
+		$("#addNodeGuideData").hide();
+		$("#addRouteGuideData").hide();
+		$("#searchGuideData").hide();
+	} else if(index == 2) {
+		$("#registerGuideData").hide();
+		$("#addNodeGuideData").show();
+		$("#addRouteGuideData").hide();
+		$("#searchGuideData").hide();
+	} else if(index == 3) {
+		$("#registerGuideData").hide();
+		$("#addNodeGuideData").hide();
+		$("#addRouteGuideData").show();
+		$("#searchGuideData").hide();
+	} else {
+		$("#registerGuideData").hide();
+		$("#addNodeGuideData").hide();
+		$("#addRouteGuideData").hide();
+		$("#searchGuideData").show();
+
+	}
+}
